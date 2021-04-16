2b81c459a0d34cb8ad5f36f8ed5664fc5629661f
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-16 16:53:30 -0700

    Added User schema and updated users route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require("../models/User");
 
 // @route     POST api/users
 // @desc      Register a user
 // @access    Public
-router.post('/', (req, res) => {
-  res.send('Sign up a user')
+router.post('/', [
+  check('name', 'Plase enter a name').not().isEmpty(),
+  check('username', 'Please enter a username').not().isEmpty(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()});
+  }
+
+  res.send('passed');
 });
 
 module.exports = router;
