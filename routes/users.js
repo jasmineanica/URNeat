ddaa222a0e6a7b62bb511f66860f8c73a60a584f
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-16 17:13:19 -0700

    User routes
    - this file now checks for user inputs and returns errors if types are
      missing from the schema that are required
    - encrypts user password using bcrypt salt and hash functions
    - added the new user to the DB

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const User = require("../models/User");
 
 // @route     POST api/users
@@ -10,13 +11,36 @@ router.post('/', [
   check('name', 'Plase enter a name').not().isEmpty(),
   check('username', 'Please enter a username').not().isEmpty(),
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array()});
   }
 
-  res.send('passed');
+  const { name, username , password } = req.body;
+
+  try {
+    let user = await User.findOne({ username: username });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists"});
+    }
+
+    user = new User({
+      name: name,
+      username: username,
+      password: password
+    });
+
+    // need to encrypt the password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+    res.send('user saved');
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
