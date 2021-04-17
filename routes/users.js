56eb186066f13d807dbd1ba99daa537f418211b6
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-16 17:38:21 -0700

    User route updates
    - used jsonwebtoken to generate a token that we want to send to the
      user when they register and when they log in
    - we want to send the user id in the payload to access that user's data

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require("../models/User");
 
 // @route     POST api/users
@@ -36,7 +38,27 @@ router.post('/', [
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
-    res.send('user saved');
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {
+        expiresIn: 360000
+      },
+      (err, token) => {
+        if (err) {
+          throw err;
+        }
+        res.json({ token });
+      }
+    );
+
   } catch(err) {
     console.error(err.message);
     res.status(500).send('Server Error');
