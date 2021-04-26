5bc2c1fca954f560f237291adf0d4ec2dab6c2fd
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-26 14:35:43 -0700

    Post Request: Saves a drink to MongoDB

--- routes/drinks.js
+++ routes/drinks.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const Drink = require("../models/Drink");
 
 // @route     GET api/drinks
 // @desc      Get all drinks
@@ -11,8 +13,55 @@ router.get('/', (req, res) => {
 // @route     POST api/drinks
 // @desc      Add new drinks
 // @access    Private
-router.post('/', (req, res) => {
-  res.send('Add drink')
+router.post('/',  [
+  check('name', 'Plase enter a drink name').not().isEmpty(),
+  check('brand', 'Please include a brand name').not().isEmpty(),
+  check('type', 'Please specify beer / wine / spirit').not().isEmpty(),
+  check('abv', 'Please add alcohol by volume').not().isEmpty()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()});
+  }
+
+  const {
+    name,
+    brand,
+    img,
+    country,
+    region,
+    abv,
+    type,
+    subtype1,
+    subtype2
+  } = req.body;
+
+
+  try {
+
+    const drinkFields = {};
+
+    drinkFields.name = name;
+    drinkFields.brand = brand;
+    drinkFields.abv = abv;
+    drinkFields.type = type;
+
+    if(img) drinkFields.img = img;
+    if(country) drinkFields.country = country;
+    if(region) drinkFields.region = region;
+    if(type) drinkFields.type = type;
+    if(subtype1) drinkFields.subtype1 = subtype1;
+    if(subtype2) drinkFields.subtype2 = subtype2;
+
+    const newDrink = new Drink(drinkFields);
+
+    const drink = await newDrink.save();
+    res.json(drink);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route     PUT api/drinks/:id
@@ -20,6 +69,15 @@ router.post('/', (req, res) => {
 // @access    Private
 router.put('/:id', (req, res) => {
   res.send('Update drink')
+  // if(name) drinkFields.name = name;
+  // if(brand) drinkFields.brand = brand;
+  // if(img) drinkFields.img = img;
+  // if(country) drinkFields.country = country;
+  // if(region) drinkFields.region = region;
+  // if(abv) drinkFields.abv = abv;
+  // if(type) drinkFields.type = type;
+  // if(subtype1) drinkFields.subtype1 = subtype1;
+  // if(subtype2) drinkFields.subtype2 = subtype2;
 });
 
 // @route     DELETE api/drinks/:id
